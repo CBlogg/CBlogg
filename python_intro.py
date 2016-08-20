6181b4f9b328a3f670087ad5de6713fe709e55bd
cb <bnf2088@gmail.com>
2016-08-20 17:06:59 +1000

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,15 @@
+if 3 > 2:
+	print('it works')
+if 5 > 2:
+	print('5 is indeed greater than 2')
+else:
+	print('5 is not greater than 2')
+	name = 'Sonia'
+	if name =='Ola':
+			print('Hey Ola')
+	elif name == 'Sonia':
+		print('Hey Sonia')
+	else:
+		print('Hey anonymous!')
+	python3 -m venv myvenv
+	
